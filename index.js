267b19d11656c61a865ac6fd38ef7109e063946e
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:03:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
+const root = ReactDOM.createRoot(document.getElemenById("root"));
+root.render(
+  <BrowserRoter>
+  <App />
+  </BrowserRouter>
+);
