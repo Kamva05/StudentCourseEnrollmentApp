53e5656cb18c94f94049fbbdfb7c81ce9f1f2f76
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:34:56 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 
 function Navbar({ total }) {
   return (
-    <nav style={{ display: "flex", gap: "20px", padding: "10px" }}>
+    <nav style={{ display: "flex", 
+                gap: "20px", 
+                padding: "10px" }}>
 <Link tp="/">Home</Link>
 <Link to "/courses">Courses</Link>
 <Link to="/enrolled">Enrolled ({total})</Link>
