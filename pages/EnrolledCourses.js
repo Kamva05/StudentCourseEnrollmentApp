959531022bc5005eeb1d7687c366908bb9210ee4
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 16:02:53 +0200

    Update EnrolledCourses.js

--- pages/EnrolledCourses.js
+++ pages/EnrolledCourses.js
@@ -1,1 +1,20 @@
+function EnrolledCourses({ enrolledCourses, removeCourse }) {
+  return (
+    <div>
+    <h2>Enrolled Courses</h2>
 
+  {enrolledCourses.length === 0 ? (
+    <p>No courses enrolled yet</p>
+   ) : (
+     enrolledCourses.map(course => (
+       <div key={course.id}>
+       <span>{course.title}</span>
+        <button onClick={() => removeCourse(course.id)}>Remove</button>
+                         </div>
+                         ))
+   )}
+  </div>
+  );
+}
+
+export default EnrolledCourses;
