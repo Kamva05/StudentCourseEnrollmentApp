759a9104e8de25e49350b521c3ef4cdb6ed20404
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:37:37 +0200

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-    <h!>Welcome to Course Enrollment App</H1>
+    <h1>Welcome to Course Enrollment App</h1>
   <p>Select courses and manage your enrollment.</p>
     </div>
   );
