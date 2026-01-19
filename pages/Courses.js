e6d2636df3ae20de9c30a787e7c5ffb887eb40b9
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:46:42 +0200

    Update Courses.js

--- pages/Courses.js
+++ pages/Courses.js
@@ -1,1 +1,29 @@
+import CourseCard from "../components/CourseCard";
 
+const courses = [ 
+  { id: 1, title: "React Basics" },
+  { id: 2, title: "Advanced JavaScript" },
+  { id: 3, title: "Node.js Fundamentals" }, 
+  { id: 4, title: "CSS Matery" },
+  { id: 5, title: "Data Structures" },
+  ];
+
+function Courses
+({ enrollCourse, EnrolledCourses }) {
+  return (
+    <div>
+    <h2>Available Courses</h2>
+  {courses.map(course => 9
+               <CourseCard
+               key={course.id}
+   course={course}
+   enrollCourse={enrollCourse}
+
+  isEnrolled={enrolledCoures.some(c => c.id === course.id)}
+  />
+  ))}
+</div>
+);
+}
+
+export default Courses;
