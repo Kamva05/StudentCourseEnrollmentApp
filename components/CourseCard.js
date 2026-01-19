d430ea1d00ccc0732204c50cbb114fc6785c9efd
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:55:45 +0200

    Update CourseCard.js

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -1,1 +1,17 @@
+function CourseCard({ course, enrollCourse, isEnrolled }) {
+  return (
+    <div style={{ border: "1px solid #ccc",
+                  margin: "10px",
+                  padding: "10px" }}>
+<h3>{course.title}</h3>
+<button onClick={() =>
+enrollCourse(course)} 
+disabled={isEnrolled}>
 
+{isEnrolled ? "Enrolled" : "Enroll"}
+</button>
+  </div>
+);
+}
+
+export default CourseCard;
