942cc70bf1d9a18379988181a7a6f8c11b832808
Kamva05 <kamva.mhlana@gmail.com>
2026-01-19 15:36:47 +0200

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,1 +1,10 @@
+function Home() {
+  return (
+    <div>
+    <h!>Welcome to Course Enrollment App</H1>
+  <p>Select courses and manage your enrollment.</p>
+    </div>
+  );
+}
 
+export default Home;
